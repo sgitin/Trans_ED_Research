91b299df7e048067a411f0cd5c06378c7603f74c
sgitin <s.q.gitin@gmail.com>
2020-05-15 16:37:40 -0700

    Update text_processing.jl

--- text_processing.jl
+++ text_processing.jl
@@ -17,9 +17,11 @@ for file in readdir()
         for i in 1:length(posts_df[:, 1])
             post = posts_df[i, 2]
             sd = StringDocument(post)
-            ## remove numbers, articles, prepositions, stop words, html tags, punctuation
+            ## remove numbers, articles, prepositions, stop words, html tags, punctuation, pronouns
             prepare!(sd, strip_numbers | strip_articles | strip_prepositions | strip_stopwords | strip_html_tags | strip_punctuation | strip_case | strip_pronouns | strip_non_letters)
+            ## stem words
             stem!(sd)
+            ## remove words that are part of a url
             remove_words!(sd, ["http", "https", "com", "www", "jpg", "imgur", "amp", "x", "b", "img", "png"])
             push!(processed_posts_df, posts_df[i, 1:2])
             processed_posts_df[i, 2] = text(sd)
